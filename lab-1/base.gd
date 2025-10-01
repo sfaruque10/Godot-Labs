fcc0454fc94f2fe771a479e89778cdbf4d671add
sfaruque10 <98576322+sfaruque10@users.noreply.github.com>
2025-09-30 20:42:25 -0400

    Update base.gd

--- lab-1/base.gd
+++ lab-1/base.gd
@@ -3,8 +3,6 @@ extends Area2D
 
 func _ready():
 	body_entered.connect(_on_body_entered)
-	#Scoring.speed_changed.connect(_on_body_entered)
-	#_on_body_entered(Scoring.player_speed)
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
@@ -12,7 +10,6 @@ func _on_body_entered(body):
 		Scoring.add_score(1) # increases score
 		Scoring.increase_speed() # increase speed everytime base is touched
 		body.speed = Scoring.player_speed
-		#var enemies = get_tree().get_nodes_in_group("Philly")
 		
 		
 		
